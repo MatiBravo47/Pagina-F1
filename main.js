d8e427f8357a3d7675140b6a65d0e2740ada44ed
MatiBravo <Matibravoneron@gmail.com>
2022-05-03 22:06:20 -0300

    JavaScript / error body

--- main.js
+++ main.js
@@ -8,4 +8,10 @@ function myFunction() {
 }
 
 var circuitos = ["Bahrein","Arabia Saudita","Australia","Emilia Romagna","Miami","Espania","Monaco","Azerbaiyan","Canada","Gran Bretania","Austria","Francia","Hungria","Belgica","Paises Bajos","Italia","Singapur","Japon","Estados Unidos","Ciudad de Mexico","Sao Paulo","Abu Dhabi"];
-var dias = [20,27,10]
+var dias = [20,27,10,24,8,22,29,12,19,3,10,24,31,28,4,11,2,9,23,30,13,20];
+var meses= ["Marzo","Marzo","Abril","Abril","Mayo","Mayo","Mayo","Junio","Junio","Julio","Julio","Julio","Julio","Agosto","Septiembre","Septiembre","Octubre","Octubre","Octubre","Noviembre","Noviembre"];
+var nombre=["Max","Daniel","Lando","Sebastian","Nicholas","Pierre","Sergio","Fernando","Charles","Lance","Kevin","Yuki","Alexander","Guanyu","Esteban","Lewis","Mick","Carlos","George","Valtteri"];
+var apellido=["Verstappen","Ricciardo","Norris","Vettel","Latifi","Gasly","Perez","Alonso","Leclerc","Stroll","Magnussen","Tsunoda","Albon","Zhou","Ocon","Hamilton","Schumacher","Sainz","Russell","Bottas"];
+var numero=[1,3,4,5,6,10,11,14,16,18,20,22,23,24,31,44,47,55,63,77];
+
+
