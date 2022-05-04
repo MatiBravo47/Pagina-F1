5edb4b083c0fcac096c9eaf25600f6db22b54a8b
MatiBravo <Matibravoneron@gmail.com>
2022-05-03 23:20:40 -0300

    no se

--- main.js
+++ main.js
@@ -13,8 +13,17 @@ var meses= ["Marzo","Marzo","Abril","Abril","Mayo","Mayo","Mayo","Junio","Junio"
 var nombre=["Max","Daniel","Lando","Sebastian","Nicholas","Pierre","Sergio","Fernando","Charles","Lance","Kevin","Yuki","Alexander","Guanyu","Esteban","Lewis","Mick","Carlos","George","Valtteri"];
 var apellido=["Verstappen","Ricciardo","Norris","Vettel","Latifi","Gasly","Perez","Alonso","Leclerc","Stroll","Magnussen","Tsunoda","Albon","Zhou","Ocon","Hamilton","Schumacher","Sainz","Russell","Bottas"];
 var numero=[1,3,4,5,6,10,11,14,16,18,20,22,23,24,31,44,47,55,63,77];
+var equipos=["Mclaren","Ferrari","Red Bull","Mercedes","Alpine","Williams","Alfa Romeo","Alpha Tauri","Aston Martin"];
 
-//document.getElementById("header").innerHTML=`
-//`
+For (let i=0; i< circuitos.length() ; i++){
+`
+<div>
+  <p>dia:${dias[i]}</p>
+  <p>mes:${meses[i]}</p>
+  <p>circuito:${circuitos[i]}</p>
+</div>
+`
+}
+
 
 
