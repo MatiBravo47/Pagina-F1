cba78ada3e10611792b53a643fca291b9a803574
MatiBravo47 <Matibravoneron@gmail.com>
2022-07-01 11:11:24 -0300

    agrego logos escuderias

--- main.js
+++ main.js
@@ -10,101 +10,121 @@ var data=[
   {Codigo:"cod1",
   nombre:"Carlos Sainz",
   equipo:"Ferrari",
+  logo:"img/logoE/Ferrari.png",
   PTS:102,
   },
   {Codigo:"cod2",
   nombre:"Charles Leclerc",
   equipo:"Ferrari",
+  logo:"img/logoE/Ferrari.png",
   PTS:126
   },
   {Codigo:"cod3",
   nombre:"Max Verstappen",
   equipo:"Red Bull Racing",
+  logo:"img/logoE/RedBull.png",
   PTS:175
   },
   {Codigo:"cod4",
   nombre:"Checo Perez",
   equipo:"Red Bull Racing",
+  logo:"img/logoE/RedBull.png",
   PTS:129
   },
   {Codigo:"cod5",
   nombre:"Lando Norris",
   equipo:"Mclaren",
+  logo:"img/logoE/Mclaren.png",
   PTS:50
   },
   {Codigo:"cod6",
   nombre:"Daniel Ricciardo",
   equipo:"Mclaren",
+  logo:"img/logoE/Mclaren.png",
   PTS:15
   },
   {Codigo:"cod7",
   nombre:"Fernando Alonso",
   equipo:"Alpine",
+  logo:"img/logoE/Alpine.png",
   PTS:18
   },
   {Codigo:"cod8",
   nombre:"Esteban Ocon",
   equipo:"Alpine",
+  logo:"img/logoE/Alpine.png",
   PTS:39
   },
   {Codigo:"cod9",
   nombre:"Kevin Magnussen",
   equipo:"Haas F1 Team",
+  logo:"img/logoE/Haas.png",
   PTS:15
   },
   {Codigo:"cod10",
   nombre:"Mick Schumacher",
   equipo:"Haas F1 Team",
+  logo:"img/logoE/Haas.png",
   PTS:0
   },
   {Codigo:"cod11",
   nombre:"Lewis Hamilton",
   equipo:"Mercedes",
+  logo:"img/logoE/Mercedes.png",
   PTS:77
   },
   {Codigo:"cod12",
   nombre:"George Russell",
   equipo:"Mercedes",
+  logo:"img/logoE/Mercedes.png",
   PTS:111
   },
   {Codigo:"cod13",
   nombre:"Yuki Tsunoda",
   equipo:"AlphaTauri",
+  logo:"img/logoE/AlphaTauri.png",
   PTS:11
   },
   {Codigo:"cod14",
   nombre:"Pierre Gasly",
   equipo:"AlphaTauri",
+  logo:"img/logoE/AlphaTauri.png",
   PTS:16
   },
   {Codigo:"cod15",
   nombre:"Valtteri Bottas",
   equipo:"Alfa Romeo",
+  logo:"img/logoE/AlfaRomeo.png",
   PTS:46
   },
   {Codigo:"cod16",
   nombre:"Zhou Gaunyu",
   equipo:"Alfa Romeo",
+  logo:"img/logoE/AlfaRomeo.png",
   PTS:5
   },
   {Codigo:"cod17",
   nombre:"Nicholas Latifi",
   equipo:"Williams",
+  logo:"img/logoE/Williams.png",
   PTS:0
   },
   {Codigo:"cod18",
   nombre:"Alexander Albon",
   equipo:"Williams",
+  logo:"img/logoE/Williams.png",
   PTS:3
   },
   {Codigo:"cod19",
   nombre:"Sebastian Vettel",
   equipo:"Aston Martin",
+  logo:"img/logoE/AstonMartin.png",
   PTS:13
   },
   {Codigo:"cod20",
   nombre:"Lance Stroll",
   equipo:"Aston Martin",
+  logo:"img/logoE/AstonMartin.png",
   PTS:3
   },
 ]
@@ -118,8 +138,10 @@ cad=`
     <p class="h1">Clasificacion mundial de pilotos<p>
     <table class="table table-hover table-dark">
       <tr>
-        <th>Nombre</th>
+        <th>Posicion</th>
+        <th>Piloto</th>
         <th>Equipo</th>
+        <th></th>
         <th>PTS</th>
       </tr>
   `
@@ -127,8 +149,10 @@ cad=`
 for (var i=0; i<data.length; i++) {
     cad+= `
         <tr>
+            <td>${i+1}</td>
             <td>${porPuntos[i].nombre}</td>
             <td>${porPuntos[i].equipo}</td>
+            <td><img src="${porPuntos[i].logo}" width="40px" ;border-style:none; ></img></td>
             <td>${porPuntos[i].PTS}</td>
         </tr>
         `
