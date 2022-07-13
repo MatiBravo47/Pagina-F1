56f056b1bcbc955eb83f4a47486e3506d9c0c668
MatiBravo47 <Matibravoneron@gmail.com>
2022-07-13 19:26:19 -0300

    agrego doc

--- main.js
+++ main.js
@@ -6,175 +6,199 @@ function myFunction() {
     x.className = "topnav";
   }
 }
-var data=[
-  {Codigo:"cod1",
+//Vector de objetos,cada objeto es un piloto de F1
+var pilotos=[
+  {
   nombre:"Carlos Sainz",
   equipo:"Ferrari",
   logo:"img/logoE/Ferrari.png",
   bandera:"img/BanderasP/Espania.jpg",
-  PTS:102,
+  foto:"./img/Pilotos/Sainz.jpg",
+  PTS:102+25,
   },
-  {Codigo:"cod2",
+  {
   nombre:"Charles Leclerc",
   equipo:"Ferrari",
   logo:"img/logoE/Ferrari.png",
   bandera:"img/BanderasP/Monaco.jpg",
-  PTS:126
+  foto:"./img/Pilotos/Lecrerc.jpg",
+  PTS:126 + 12
   },
-  {Codigo:"cod3",
+  {
   nombre:"Max Verstappen",
-  equipo:"Red Bull Racing",
+  equipo:"Red Bull",
   logo:"img/logoE/RedBull.png",
-  bandera:"img/BanderasP/Monaco.jpg",
-  PTS:175
+  bandera:"img/BanderasP/Holanda.jpg",
+  foto:"./img/Pilotos/Max Verstappen.jpg",
+  PTS:175 + 6
   },
-  {Codigo:"cod4",
+  {
   nombre:"Checo Perez",
-  equipo:"Red Bull Racing",
+  equipo:"Red Bull",
   logo:"img/logoE/RedBull.png",
   bandera:"img/BanderasP/Mexico.jpg",
-  PTS:129
+  foto:"./img/Pilotos/Perez.jpg",
+  PTS:129 + 18
   },
-  {Codigo:"cod5",
+  {
   nombre:"Lando Norris",
   equipo:"Mclaren",
   logo:"img/logoE/Mclaren.png",
-  bandera:"img/BanderasP/EEUU.jpg",
-  PTS:50
+  bandera:"img/BanderasP/ReinoUnido.jpg",
+  foto:"./img/Pilotos/Norris.jpg",
+  PTS:50 + 8
   },
-  {Codigo:"cod6",
+  {
   nombre:"Daniel Ricciardo",
   equipo:"Mclaren",
   logo:"img/logoE/Mclaren.png",
   bandera:"img/BanderasP/Australia.jpg",
+  foto:"./img/Pilotos/Ricciardo.jpg",
   PTS:15
   },
-  {Codigo:"cod7",
+  {
   nombre:"Fernando Alonso",
   equipo:"Alpine",
   logo:"img/logoE/Alpine.png",
   bandera:"img/BanderasP/Espania.jpg",
-  PTS:18
+  foto:"./img/Pilotos/Alonso.jpg",
+  PTS:18 + 10
   },
-  {Codigo:"cod8",
+  {
   nombre:"Esteban Ocon",
   equipo:"Alpine",
   logo:"img/logoE/Alpine.png",
   bandera:"img/BanderasP/Francia.jpg",
+  foto:"./img/Pilotos/Ocon.jpg",
   PTS:39
   },
-  {Codigo:"cod9",
+  {
   nombre:"Kevin Magnussen",
   equipo:"Haas F1 Team",
   logo:"img/logoE/Haas.png",
   bandera:"img/BanderasP/Dinamarca.jpg",
-  PTS:15
+  foto:"./img/Pilotos/Magnussen.jpg",
+  PTS:15 + 1
   },
-  {Codigo:"cod10",
+  {
   nombre:"Mick Schumacher",
   equipo:"Haas F1 Team",
   logo:"img/logoE/Haas.png",
   bandera:"img/BanderasP/Alemania.jpg",
-  PTS:0
+  foto:"./img/Pilotos/Schumacher.jpg",
+  PTS:0+4
   },
-  {Codigo:"cod11",
+  {
   nombre:"Lewis Hamilton",
   equipo:"Mercedes",
   logo:"img/logoE/Mercedes.png",
-  bandera:"img/BanderasP/EEUU.jpg",
-  PTS:77
+  bandera:"img/BanderasP/ReinoUnido.jpg",
+  foto:"./img/Pilotos/Hamilton.jpg",
+  PTS:77 + 16
   },
-  {Codigo:"cod12",
+  {
   nombre:"George Russell",
   equipo:"Mercedes",
   logo:"img/logoE/Mercedes.png",
-  bandera:"img/BanderasP/EEUU.jpg",
+  bandera:"img/BanderasP/ReinoUnido.jpg",
+  foto:"./img/Pilotos/Russell.jpg",
   PTS:111
   },
-  {Codigo:"cod13",
+  {
   nombre:"Yuki Tsunoda",
   equipo:"AlphaTauri",
   logo:"img/logoE/AlphaTauri.png",
   bandera:"img/BanderasP/Japon.jpg",
+  foto:"./img/Pilotos/Tsunoda.jpg",
   PTS:11
   },
-  {Codigo:"cod14",
+  {
   nombre:"Pierre Gasly",
   equipo:"AlphaTauri",
   logo:"img/logoE/AlphaTauri.png",
   bandera:"img/BanderasP/Francia.jpg",
+  foto:"./img/Pilotos/Gasly.jpg",
   PTS:16
   },
-  {Codigo:"cod15",
+  {
   nombre:"Valtteri Bottas",
   equipo:"Alfa Romeo",
   logo:"img/logoE/AlfaRomeo.png",
   bandera:"img/BanderasP/Finlandia.jpg",
+  foto:"./img/Pilotos/Bottas.jpg",
   PTS:46
   },
-  {Codigo:"cod16",
+  {
   nombre:"Zhou Gaunyu",
   equipo:"Alfa Romeo",
   logo:"img/logoE/AlfaRomeo.png",
   bandera:"img/BanderasP/China.jpg",
+  foto:"./img/Pilotos/Gaunyu.jpg",
   PTS:5
   },
-  {Codigo:"cod17",
+  {
   nombre:"Nicholas Latifi",
   equipo:"Williams",
   logo:"img/logoE/Williams.png",
   bandera:"img/BanderasP/Canada.jpg",
+  foto:"./img/Pilotos/Latifi.jpg",
   PTS:0
   },
-  {Codigo:"cod18",
+  {
   nombre:"Alexander Albon",
   equipo:"Williams",
   logo:"img/logoE/Williams.png",
   bandera:"img/BanderasP/Tailandia.jpg",
+  foto:"./img/Pilotos/Albon.jpg",
   PTS:3
   },
-  {Codigo:"cod19",
+  {
   nombre:"Sebastian Vettel",
   equipo:"Aston Martin",
   logo:"img/logoE/AstonMartin.png",
   bandera:"img/BanderasP/Alemania.jpg",
-  PTS:13
+  foto:"./img/Pilotos/Vettel.jpg",
+  PTS:13 + 2 
   },
-  {Codigo:"cod20",
+  {
   nombre:"Lance Stroll",
   equipo:"Aston Martin",
   logo:"img/logoE/AstonMartin.png",
   bandera:"img/BanderasP/Canada.jpg",
+  foto:"./img/Pilotos/Stroll.jpg",
   PTS:3
   },
 ]
-var porPuntos = data.slice(0);
+//Funcion que arregla en orden decreciente por puntos 
+var porPuntos = pilotos.slice(0);
 porPuntos.sort(function(a,b) {
   return b.PTS - a.PTS;
 });
+
+
+//Codigo tabla de posiciones 
 if ( document.getElementById( "idtabla" )) {
-  //la tabla tiene un encabezado fijo
+//encabezado fijo tabla
 cad=`
     <p class="h1">Clasificacion mundial de pilotos<p>
     <table class="table table-hover table-dark">
       <tr>
-        <th>Posicion</th>
-        <th></th>
+        <th>Pos</th>
         <th>Piloto</th>
         <th>Equipo</th>
-        <th></th>
         <th>PTS</th>
       </tr>
   `
 //y ademas tiene un contenido que varia segun su data
-for (var i=0; i<data.length; i++) {
+for (var i=0; i<pilotos.length; i++) {
     cad+= `
         <tr>
             <td>${i+1}</td>
-            <td><img src="${porPuntos[i].bandera}" width="40px" ;border-style:none; ></img></td> 
-            <td>${porPuntos[i].nombre}</td>
-            <td>${porPuntos[i].equipo}</td>
-            <td><img src="${porPuntos[i].logo}" width="40px" ;border-style:none; ></img></td>
+            <td><img src="${porPuntos[i].bandera}" width="40px" ;border-style:none; ></img> 
+            ${porPuntos[i].nombre}</td>
+            <td><img src="${porPuntos[i].logo}" width="40px" ;border-style:none; ></img>
+            ${porPuntos[i].equipo}
+            </td>
             <td>${porPuntos[i].PTS}</td>
         </tr>
         `
@@ -187,6 +211,16 @@ cad+=`
 document.getElementById("idtabla").innerHTML = cad;
 }
 
+//Codigo para pilotos 
+
+//for (var col = 0; col < 4; col++ ) {
+//  for (var fil = 0; fil < 5; fil++){
+//    pil+=`<div class="${equipo[i]} contenedor">
+//      <span class="Apellido White"><b>${nombre[i]}</b></span>
+//
+//
+//  }
+//}
 
 
 
