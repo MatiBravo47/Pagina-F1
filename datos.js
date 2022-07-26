68648c8afbc2b2bd094973d4341a531d75fb323a
MatiBravo47 <matibravoneron@gmail.com>
2022-07-26 17:49:45 -0300

    add vuejs

--- datos.js
+++ datos.js
@@ -60,6 +60,7 @@ app=new Vue({
             bandera:"img/BanderasP/Espania.jpg",
             foto:"./img/Pilotos/Sainz.jpg",
             PTS:102+25,
+            gradiente:"gradFerrari" 
             },
             {
             nombre:"Charles Leclerc",
@@ -67,7 +68,8 @@ app=new Vue({
             logo:"img/LogoE/Ferrari.png",
             bandera:"img/BanderasP/Monaco.jpg",
             foto:"./img/Pilotos/Lecrerc.jpg",
-            PTS:126 + 12
+            PTS:126 + 12,
+            gradiente:"gradFerrari" 
             },
             {
             nombre:"Max Verstappen",
@@ -75,7 +77,8 @@ app=new Vue({
             logo:"img/LogoE/RedBull.png",
             bandera:"img/BanderasP/Holanda.jpg",
             foto:"./img/Pilotos/Max Verstappen.jpg",
-            PTS:175 + 6
+            PTS:175 + 6,
+            gradiente:"gradRedBull"
             },
             {
             nombre:"Checo Perez",
@@ -83,7 +86,8 @@ app=new Vue({
             logo:"img/LogoE/RedBull.png",
             bandera:"img/BanderasP/Mexico.jpg",
             foto:"./img/Pilotos/Perez.jpg",
-            PTS:129 + 18
+            PTS:129 + 18,
+            gradiente:"gradRedBull"
             },
             {
             nombre:"Lando Norris",
@@ -91,7 +95,8 @@ app=new Vue({
             logo:"img/LogoE/Mclaren.png",
             bandera:"img/BanderasP/ReinoUnido.jpg",
             foto:"./img/Pilotos/Norris.jpg",
-            PTS:50 + 8
+            PTS:50 + 8,
+            gradiente:"gradMclaren"
             },
             {
             nombre:"Daniel Ricciardo",
@@ -99,7 +104,8 @@ app=new Vue({
             logo:"img/LogoE/Mclaren.png",
             bandera:"img/BanderasP/Australia.jpg",
             foto:"./img/Pilotos/Ricciardo.jpg",
-            PTS:15
+            PTS:15,
+            gradiente:"gradMclaren"
             },
             {
             nombre:"Fernando Alonso",
@@ -107,7 +113,8 @@ app=new Vue({
             logo:"img/LogoE/Alpine.png",
             bandera:"img/BanderasP/Espania.jpg",
             foto:"./img/Pilotos/Alonso.jpg",
-            PTS:18 + 10
+            PTS:18 + 10,
+            gradiente:"gradAlpine"
             },
             {
             nombre:"Esteban Ocon",
@@ -115,7 +122,8 @@ app=new Vue({
             logo:"img/LogoE/Alpine.png",
             bandera:"img/BanderasP/Francia.jpg",
             foto:"./img/Pilotos/Ocon.jpg",
-            PTS:39
+            PTS:39,
+            gradiente:"gradAlpine"
             },
             {
             nombre:"Kevin Magnussen",
@@ -123,7 +131,8 @@ app=new Vue({
             logo:"img/LogoE/Haas.png",
             bandera:"img/BanderasP/Dinamarca.jpg",
             foto:"./img/Pilotos/Magnussen.jpg",
-            PTS:15 + 1
+            PTS:15 + 1,
+            gradiente:"gradHaas"
             },
             {
             nombre:"Mick Schumacher",
@@ -131,7 +140,8 @@ app=new Vue({
             logo:"img/LogoE/Haas.png",
             bandera:"img/BanderasP/Alemania.jpg",
             foto:"./img/Pilotos/Schumacher.jpg",
-            PTS:0+4
+            PTS:0+4,
+            gradiente:"gradHaas"
             },
             {
             nombre:"Lewis Hamilton",
@@ -139,7 +149,8 @@ app=new Vue({
             logo:"img/LogoE/Mercedes.png",
             bandera:"img/BanderasP/ReinoUnido.jpg",
             foto:"./img/Pilotos/Hamilton.jpg",
-            PTS:77 + 16
+            PTS:77 + 16,
+            gradiente:"gradMercedes" 
             },
             {
             nombre:"George Russell",
@@ -147,7 +158,8 @@ app=new Vue({
             logo:"img/LogoE/Mercedes.png",
             bandera:"img/BanderasP/ReinoUnido.jpg",
             foto:"./img/Pilotos/Russell.jpg",
-            PTS:111
+            PTS:111,
+            gradiente:"gradMercedes" 
             },
             {
             nombre:"Yuki Tsunoda",
@@ -155,7 +167,8 @@ app=new Vue({
             logo:"img/LogoE/AlphaTauri.png",
             bandera:"img/BanderasP/Japon.jpg",
             foto:"./img/Pilotos/Tsunoda.jpg",
-            PTS:11
+            PTS:11,
+            gradiente:"gradAlphaTauri"
             },
             {
             nombre:"Pierre Gasly",
@@ -163,7 +176,8 @@ app=new Vue({
             logo:"img/LogoE/AlphaTauri.png",
             bandera:"img/BanderasP/Francia.jpg",
             foto:"./img/Pilotos/Gasly.jpg",
-            PTS:16
+            PTS:16,
+            gradiente:"gradAlphaTauri"
             },
             {
             nombre:"Valtteri Bottas",
@@ -171,7 +185,8 @@ app=new Vue({
             logo:"img/LogoE/AlfaRomeo.png",
             bandera:"img/BanderasP/Finlandia.jpg",
             foto:"./img/Pilotos/Bottas.jpg",
-            PTS:46
+            PTS:46,
+            gradiente:"gradAlfaRomeo"
             },
             {
             nombre:"Zhou Gaunyu",
@@ -179,7 +194,8 @@ app=new Vue({
             logo:"img/LogoE/AlfaRomeo.png",
             bandera:"img/BanderasP/China.jpg",
             foto:"./img/Pilotos/Gaunyu.jpg",
-            PTS:5
+            PTS:5,
+            gradiente:"gradAlfaRomeo"
             },
             {
             nombre:"Nicholas Latifi",
@@ -187,7 +203,8 @@ app=new Vue({
             logo:"img/LogoE/Williams.png",
             bandera:"img/BanderasP/Canada.jpg",
             foto:"./img/Pilotos/Latifi.jpg",
-            PTS:0
+            PTS:0,
+            gradiente:"gradWilliams"
             },
             {
             nombre:"Alexander Albon",
@@ -195,7 +212,8 @@ app=new Vue({
             logo:"img/LogoE/Williams.png",
             bandera:"img/BanderasP/Tailandia.jpg",
             foto:"./img/Pilotos/Albon.jpg",
-            PTS:3
+            PTS:3,
+            gradiente:"gradWilliams"
             },
             {
             nombre:"Sebastian Vettel",
@@ -203,7 +221,8 @@ app=new Vue({
             logo:"img/LogoE/AstonMartin.png",
             bandera:"img/BanderasP/Alemania.jpg",
             foto:"./img/Pilotos/Vettel.jpg",
-            PTS:13 + 2 
+            PTS:13 + 2,
+            gradiente:"gradAstonMartin" 
             },
             {
             nombre:"Lance Stroll",
@@ -211,7 +230,8 @@ app=new Vue({
             logo:"img/LogoE/AstonMartin.png",
             bandera:"img/BanderasP/Canada.jpg",
             foto:"./img/Pilotos/Stroll.jpg",
-            PTS:3
+            PTS:3,
+            gradiente:"gradAstonMartin"
             }]
     }
 })
